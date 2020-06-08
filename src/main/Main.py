f25c8ae2698ef35957b5a12329f4487cb70bd8b4
Natán Fernandez de Castro <48105518+natanfdecastro@users.noreply.github.com>
2020-06-08 15:10:58 -0600

    [*] _pycache_ a .gitignore [+] directorio de resources

--- src/main/Main.py
+++ src/main/Main.py
@@ -1,8 +1,13 @@
+import sys
+
 from AnalizadorLexico import Escaner
 
 # Comando de ejecucion: python3 Main.py <programa.txt>
 
-archivo = open("programa.txt")
+# Editar la ruta según computadora que se ejecuta
+ruta = "C:/Users/windows10/Documents/TEC/I Semestre - 2020/Compiladores E Intérpretes/PP1_Analizador_Lexico_IC5701/src/resources/"
+
+archivo = open(ruta + sys.argv[1],'r')
 
 programa = archivo.read()
 
