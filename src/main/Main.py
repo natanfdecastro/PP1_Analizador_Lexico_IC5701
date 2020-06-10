82c83c24c1441a26831fc7fb701f3d87de085cd5
Emanuelle <emanuellejs@hotmail.es>
2020-06-09 22:14:40 -0600

    Se arreglaron los cambios correspondientes

--- src/main/Main.py
+++ src/main/Main.py
@@ -5,13 +5,17 @@ from AnalizadorLexico import Escaner
 
 # Comando de ejecucion: python3 Main.py <programa.txt>
 
-
+# Obtiene el path actual donde se encuentra el Main.py, este directorio lo separa y lo mete en una lista
 path = os.getcwd().split("\\")
+
+# Este va ser el nuevo path para buscar las pruebas
 new_path = ""
 
+# Insertar todos los paths menos el último, exceptuando la carpeta main
 for i in range(len(path)-1):
     new_path += path[i] + '/'
 
+# Concatena la carpeta resources al path nuevo, esto para que apunte a una nueva carpeta
 new_path += "resources/"
 
 # Editar la ruta según computadora que se ejecuta
