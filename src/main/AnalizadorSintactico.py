f2f293ec3f1f208457fa76767a284ac786c9418a
faoalvarado5 <44043200+faoalvarado5@users.noreply.github.com>
2020-07-13 21:33:18 -0600

    DeclMeotdo
    Se creo el parseo de DeclMetodo

--- src/main/AnalizadorSintactico.py
+++ src/main/AnalizadorSintactico.py
@@ -30,6 +30,60 @@ class AnalizadorSintactico():
                 self.generar_error()
 
 
+    def parsear_DeclMetodo(self, puntero_token):
 
+        if(self.comparar("PUBLICO", puntero_token)):
+            subarbol.agregar_nodo(self.crear_nodo(puntero_token))
+            puntero_token += 1
+            if (self.comparar("TIPO", puntero_token)):
+                puntero_token += 1
+                arbol_aux, nuevo_puntero = self.parsear_tipo(puntero_token)
+                if (self.comparar("IDEN", puntero_token)):
+                    puntero_token += 1
+                    if (self.comparar("PAREN_IZQUIERDO", puntero_token)):
+                        puntero_token += 1
+                        if (self.comparar("LISTA_FORMAL", puntero_token)):
+                            puntero_token += 1
+                            arbol_aux, nuevo_puntero = self.parsear_lista_formal(puntero_token)
+                            if (self.comparar("PAREN_DERECHO", puntero_token)):
+                                puntero_token += 1
+                                if (self.comparar("CORCHETE_IZQUIERDO", puntero_token)):
+                                    puntero_token += 1
 
+                                    subarbol_aux, nuevo_puntero = self.parsear_accion(puntero_token)
+                                    if (self.validar_parseo(subarbol_aux)):
+                                        subarbol.agregar_nodo(subarbol_aux)
+                                        puntero_token = nuevo_puntero
 
+                                        while (True):
+                                            subarbol_aux, nuevo_puntero = self.parsear_decl_var(puntero_token)
+                                            if (self.validar_parseo(subarbol_aux)):
+                                                subarbol.agregar_nodo(subarbol_aux)
+                                                puntero_token = nuevo_puntero
+                                            else:
+                                                break
+
+                                        subarbol_aux, nuevo_puntero = self.parsear_accion(puntero_token)
+                                        if (self.validar_parseo(subarbol_aux)):
+                                            subarbol.agregar_nodo(subarbol_aux)
+                                            puntero_token = nuevo_puntero
+
+                                            while (True):
+                                                subarbol_aux, nuevo_puntero = self.parsear_declaracion(puntero_token)
+                                                if (self.validar_parseo(subarbol_aux)):
+                                                    subarbol.agregar_nodo(subarbol_aux)
+                                                    puntero_token = nuevo_puntero
+                                                else:
+                                                    break
+
+                                            if (self.comparar("RETORNAR", puntero_token)):
+                                                puntero_token += 1
+                                                if (self.comparar("EXPREN", puntero_token)):
+                                                    puntero_token += 1
+                                                    arbol_aux, nuevo_puntero = self.parsear_expren(puntero_token)
+                                                    if (self.comparar("PUNTO_Y_COMA", puntero_token)):
+                                                        puntero_token += 1
+                                                        if (self.comparar("CORCHETE_DERECHO", puntero_token)):
+                                                            puntero_token += 1
+        self.calcular_error(puntero_token)
+        return [], -1
